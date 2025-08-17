be03e965b02a917e5ede3ab584c68e0bb0a283cb
Williams B. Gomes <williamsgomes45@gmail.com>
2025-08-17 11:07:49 -0300

    feat: :sparkles: Mock da lista de fotos

---
+++ src/contexts/photos/components/photos-list.tsx
@@ -0,0 +1,52 @@
+import { Skeleton } from "../../../components/skeleton";
+import { Text } from "../../../components/text";
+import type { Photo } from "../models/photo";
+import { PhotoWidget } from "./photo-widget";
+
+interface PhotosListProps {
+	photos: Photo[];
+	loading?: boolean;
+}
+
+export function PhotosList({ photos, loading }: PhotosListProps) {
+	return (
+		<div className="space-y-6">
+			<Text
+				as="div"
+				variant="paragraph-large"
+				className="flex items-center justify-end gap-1 text-accent-span"
+			>
+				Total:{" "}
+				{!loading ? (
+					<div>{photos.length}</div>
+				) : (
+					<Skeleton className="w-6 h-6" />
+				)}
+			</Text>
+
+			{!loading && photos.length > 0 && (
+				<div className="grid grid-cols-5 gap-9">
+					{photos.map((photo) => (
+						<PhotoWidget key={photo.id} photo={photo} />
+					))}
+				</div>
+			)}
+			{loading && (
+				<div className="grid grid-cols-5 gap-9">
+					{Array.from({ length: 10 }).map((_, index) => (
+						<PhotoWidget
+							key={`photo-loading-${index + 1}`}
+							loading
+							photo={{} as Photo}
+						/>
+					))}
+				</div>
+			)}
+			{!loading && photos.length === 0 && (
+				<div className="flex items-center justify-center h-full">
+					<Text variant="paragraph-large">Nenhuma foto encontrada!</Text>
+				</div>
+			)}
+		</div>
+	);
+}
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,40 +1,23 @@
 import { Container } from "../components/container";
-import { PhotoWidget } from "../contexts/photos/components/photo-widget";
-import type { Photo } from "../contexts/photos/models/photo";
+import { PhotosList } from "../contexts/photos/components/photos-list";
 
 export function PageHome() {
 	return (
 		<Container>
-			<div className="grid grid-cols-5 gap-9">
-				{Array.from({ length: 4 }).map((_, index) => {
-					const photoId = String(index + 1);
-					return (
-						<PhotoWidget
-							key={`photo-${index + 1}-${photoId}`}
-							photo={{
-								id: photoId,
-								title: `Photo ${index + 1}`,
-								imageId: `portrait-tower.png`,
-								albums: [
-									{ id: "1", title: "Album 1" },
-									{ id: "2", title: "Album 2" },
-								],
-							}}
-						/>
-					);
-				})}
-				<PhotoWidget
-					photo={
-						{
-							albums: [
-								{ id: "1", title: "Album 1" },
-								{ id: "2", title: "Album 2" },
-							],
-						} as Photo
-					}
-					loading
-				/>
-			</div>
+			<PhotosList
+				photos={[
+					{
+						id: "123",
+						title: "Olá mundo!",
+						imageId: "portrait-tower.png",
+						albums: [
+							{ id: "3421", title: "Album 1" },
+							{ id: "123", title: "Album 2" },
+							{ id: "456", title: "Album 3" },
+						],
+					},
+				]}
+			></PhotosList>
 		</Container>
 	);
 }
