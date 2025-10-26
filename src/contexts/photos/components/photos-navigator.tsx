d17cca69f7984f6fcbe694f716bd20263564a969
Williams B. Gomes <williamsgomes45@gmail.com>
2025-10-25 23:41:48 -0300

    chore: :sparkles: Mock da página de detalhes da foto - Header da página

---
+++ src/contexts/photos/components/photos-navigator.tsx
@@ -0,0 +1,56 @@
+import cx from "classnames";
+import type { ComponentProps } from "react";
+import { useNavigate } from "react-router";
+import ArrowLeftIcon from "../../../assets/icons/chevron-left.svg?react";
+import ArrowRightIcon from "../../../assets/icons/chevron-right.svg?react";
+import { Button } from "../../../components/button";
+import { ButtonIcon } from "../../../components/button-icon";
+import { Skeleton } from "../../../components/skeleton";
+
+interface PhotosNavigatorProps extends ComponentProps<"div"> {
+	previusPhotoId?: string;
+	nextPhotoId?: string;
+	loading?: boolean;
+}
+
+export function PhotosNavigator({
+	previusPhotoId,
+	nextPhotoId,
+	loading,
+	className,
+	...props
+}: PhotosNavigatorProps) {
+	const navigate = useNavigate();
+
+	return (
+		<div className={cx("flex gap-2", className)} {...props}>
+			{!loading ? (
+				<>
+					<ButtonIcon
+						icon={ArrowLeftIcon}
+						variant="secondary"
+						disabled={!previusPhotoId}
+						onClick={() => {
+							navigate(`/fotos/${previusPhotoId}`);
+						}}
+					/>
+					<Button
+						icon={ArrowRightIcon}
+						variant="secondary"
+						disabled={!nextPhotoId}
+						onClick={() => {
+							navigate(`/fotos/${nextPhotoId}`);
+						}}
+					>
+						Próxima Imagem
+					</Button>
+				</>
+			) : (
+				<>
+					<Skeleton className="w-10 h-10" />
+					<Skeleton className="w-20 h-10" />
+				</>
+			)}
+		</div>
+	);
+}
--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,14 +1,33 @@
-import { useParams } from "react-router";
+import { Container } from "../components/container";
+import { Skeleton } from "../components/skeleton";
 import { Text } from "../components/text";
+import { PhotosNavigator } from "../contexts/photos/components/photos-navigator";
+import type { Photo } from "../contexts/photos/models/photo";
 
 export function PagePhotoDetails() {
-	const { id } = useParams();
+	const isLoadingPhoto = false;
+	const photo = {
+		id: "123",
+		title: "Olá mundo!",
+		imageId: "portrait-tower.png",
+		albums: [
+			{ id: "3421", title: "Album 1" },
+			{ id: "123", title: "Album 2" },
+			{ id: "456", title: "Album 3" },
+		],
+	} as Photo;
 
 	return (
-		<>
-			<Text variant="heading-medium">Página de detalhes da foto</Text>
-			<hr />
-			<Text variant="heading-medium">ID da foto: {id}</Text>
-		</>
+		<Container>
+			<header className="flex items-center justify-between gap-8 mb-8">
+				{!isLoadingPhoto ? (
+					<Text variant="heading-large">{photo.title}</Text>
+				) : (
+					<Skeleton className="w-48 h-8" />
+				)}
+
+				<PhotosNavigator />
+			</header>
+		</Container>
 	);
 }
