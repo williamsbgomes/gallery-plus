1e0d8fbfae8a1b795d12e3bda513862d106a2f24
Williams B. Gomes <williamsgomes45@gmail.com>
2025-10-26 00:05:57 -0300

    Mock da página de detalhes da foto - Imagem e botão de excluir

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,4 +1,6 @@
+import { Button } from "../components/button";
 import { Container } from "../components/container";
+import { ImagePreview } from "../components/image-preview";
 import { Skeleton } from "../components/skeleton";
 import { Text } from "../components/text";
 import { PhotosNavigator } from "../contexts/photos/components/photos-navigator";
@@ -28,6 +30,24 @@ export function PagePhotoDetails() {
 
 				<PhotosNavigator />
 			</header>
+			<div className="grid grid-cols-[21rem] gap-24">
+				<div className="space-y-3">
+					{!isLoadingPhoto ? (
+						<ImagePreview
+							src={`/images/${photo.imageId}`}
+							title={photo.title}
+							imageClassName="h-[21rem]"
+						/>
+					) : (
+						<Skeleton className="h-[21rem]" />
+					)}
+					{!isLoadingPhoto ? (
+						<Button variant="destructive">Excluir</Button>
+					) : (
+						<Skeleton className="w-20 h-10" />
+					)}
+				</div>
+			</div>
 		</Container>
 	);
 }
